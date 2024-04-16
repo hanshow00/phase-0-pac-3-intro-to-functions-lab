98062ea5f38e5463edc3dad635799e79ebe67fbc
Hans How <hanshow00@gmail.com>
2024-04-15 21:50:51 -0500

    functions lab

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout (string) {
+    console.log(string.toUpperCase( ));
+  }
+  function logWhisper (string) {
+    console.log(string.toLowerCase( ));
+  }
+  function sayHiToHeadphonedRoommate (string) {
+    var cantHear = "I can't hear you!";
+    var yesHear = "YES INDEED!";
+    var lovHear = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+      return cantHear;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return yesHear;
+    }
+    else if ("Let's have dinner together!" === string) {
+      return lovHear;
+    }
+  }
